1ac3bee5496d75381578d168501c0689312271b5
ryanycw <ryan@ryanycw.dev>
2025-08-05 23:25:22 +0800

    feat: implement banned token store with HashSet backing and associated tests in auth-service

--- auth-service/src/domain/data_stores.rs
+++ auth-service/src/domain/data_stores.rs
@@ -20,3 +20,14 @@ pub enum UserStoreError {
     InvalidCredentials,
     UnexpectedError,
 }
+
+#[async_trait::async_trait]
+pub trait BannedTokenStore {
+    async fn store_token(&mut self, token: String) -> Result<(), BannedTokenStoreError>;
+    async fn contains_token(&self, token: &str) -> Result<bool, BannedTokenStoreError>;
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BannedTokenStoreError {
+    UnexpectedError,
+}
--- auth-service/src/services/mod.rs
+++ auth-service/src/services/mod.rs
@@ -1,7 +1,9 @@
+pub mod banned_token_store;
 pub mod hashmap_login_attempt_store;
 pub mod hashmap_user_store;
 pub mod recaptcha_service;
 
+pub use banned_token_store::*;
 pub use hashmap_login_attempt_store::*;
 pub use hashmap_user_store::*;
 pub use recaptcha_service::*;
